346c00925458ceee25c3af294a6d1a8d95958e5f
szp <2593119609@qq.com>
2020-04-09 22:06:02 +0800

    finish get cntour

--- test_post_youdao.py
+++ test_post_youdao.py
@@ -1,14 +1,20 @@
+import unittest
+from unittest import mock
+
+#from post_youdao import *
+
 class PostYoudaoTest(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True,True)
 
     def test_get_ts(self):
-        import time
-        t=time.time()
-        ts=str(int(round(t * 1000)))
-        print(ts)
-        get_ts=mock.Mok(return_value='1584684435788')
-        self.assertEqual('1584684435788',get_ts())
+        get_ts=mock.Mock(return_value='1584684880395')
+        self.assertEqual('1584684880395',get_ts())
+
+    def test_get_salt(self):
+        get_salt = mock.Mock(return_value='1584684880395')
+        self.assertEqual('1584684880395',get_salt())
+
 
 if __name__=='__main__':
     unittest.main()
