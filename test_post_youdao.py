83cecbacb06a472746cdc314125edda2034551a0
szp <2593119609@qq.com>
2020-04-09 22:54:30 +0800

    作业4 finished slat

--- test_post_youdao.py
+++ test_post_youdao.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest import mock
 
-#from post_youdao import *
+from post_youdao import *
 
 class PostYoudaoTest(unittest.TestCase):
     def test_something(self):
@@ -12,13 +12,15 @@ class PostYoudaoTest(unittest.TestCase):
         #t=time.time()
         #ts=str(int(round(t * 1000)))
         #prrint(ts)
-
-        get_ts=mock.Mock(return_value='1584684880395')
-        self.assertEqual('1584684880395',get_ts())
+        get_ts=mock.Mock(return_value='15846848803956')
+        self.assertEqual('15846848803956',get_ts())
 
     def test_get_salt(self):
-        get_salt = mock.Mock(return_value='1584684880395')
-        self.assertEqual('1584684880395',get_salt())
+        get_salt = mock.Mock(return_value='15846848803956')
+        self.assertEqual('15846848803956',get_salt())
+
+    def test_get_sign(self):
+        self.assertEqual('4cf44da69384da8fb5c2364a31b22380',get_sign())
 
 
 if __name__=='__main__':
